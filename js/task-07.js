f13c86d5b76003e287efb881932407e6006f853c
Mykola Fedotov <fedotovns@gmail.com>
2023-10-16 09:37:50 +0200

    task07 - fixed defaut value for control element

--- js/task-07.js
+++ js/task-07.js
@@ -5,6 +5,9 @@ const refs = {
 
 refs.input.addEventListener("input", handleInput);
 
+refs.input.value = refs.input.min; //set control's default value
+
 function handleInput(e) {
+  console.log(e.currentTarget.value);
   return (refs.span.style.fontSize = `${e.currentTarget.value}px`);
 }
